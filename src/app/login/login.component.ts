f04875cdefeb4a7652c5a1c105c0e1de53c879d4
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-10-02 05:10:57 +0200

    Level 1 Exercises 5 & 7. Completed.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { SignupComponent } from "../signup/signup.component";
 import { IUser } from "src/model/IUser";
+import { UsersService } from "src/services/users.service";
+import { checkPasswordValidator } from "src/utils/CustomValidators";
 
 @Component({
-    selector: "app-login",
+    selector: 'app-login',
     standalone: true,
     imports: [CommonModule, ReactiveFormsModule],
-    templateUrl: "./login.component.html",
+    templateUrl: './login.component.html',
     styles: [
-        ".close-btn svg { width: 1.4em; height: 1.4em}",
-        ":host { min-height: 100vh; display: flex; flex-direction: column}",
+        '.close-btn svg { width: 1.4em; height: 1.4em}',
+        ':host { min-height: 100vh; display: flex; flex-direction: column}',
         `
             input:-webkit-autofill,
             input:-webkit-autofill:focus {
@@ -23,78 +25,130 @@ import { IUser } from "src/model/IUser";
 })
 export class LoginComponent implements OnInit {
     protected loginForm: FormGroup = this.formBuilder.group({});
+    private _user: IUser = { email: '' };
+    @Input()
+    set user(value: IUser) {
+        this._user = value;
+    }
+    get user(): IUser {
+        return this._user;
+    }
+    protected pwdState = {
+        type: ['password', 'text'],
+        svg: ['eye-slash', 'eye'],
+        alt: [
+            'The password text is not visible',
+            'The password text is visible',
+        ],
+        state: 0,
+    };
+    @ViewChild('confirmation', { static: true }) confirmationRef:
+        | ElementRef
+        | undefined;
+    @ViewChild('error', { static: true }) errorRef:
+        | ElementRef
+        | undefined;
 
     constructor(
         public activeModal: NgbActiveModal,
         private formBuilder: FormBuilder,
-        private modalService: NgbModal
+        private modalService: NgbModal,
+        private usersService: UsersService
     ) {}
 
     ngOnInit(): void {
         const formGroup = {
-            email: new FormControl("", {
+            email: new FormControl('', {
                 validators: [Validators.required, Validators.email],
-                // asyncValidators: [
-                //     this.budgetExistsValidator.validate.bind(
-                //         this.budgetExistsValidator
-                //     ),
-                // ],
+            }),
+            password: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(8),
+                    checkPasswordValidator(),
+                ],
             }),
         };
         this.loginForm = this.formBuilder.group(
-            formGroup /* , {
-            validators: (control: AbstractControl) => {
-                const website = control.get("website")?.value;
-                const pages = control.get("pages")?.value;
-                const languages = control.get("languages")?.value;
-                if (!website) return null;
-                if (pages > 0 && languages > 0) return null;
-                return { detail: true };
-            },
-        } */
+            formGroup
         );
 
         this.loginForm.reset();
+
+        if (this.user.email != null)
+            this.loginForm.get('email')!.setValue(this.user.email);
+        if (this.user.password != null)
+            this.loginForm.get('password')!.setValue(this.user.password);
     }
 
     protected getError = (field: string) => {
         const errors = this.loginForm.get(field)?.errors ?? {};
-        const subject = "The email";
-        /* field === "name"
-                ? "El nombre del presupuesto"
-                : field === "customer"
-                ? "El nombre del cliente"
-                : "La fecha del presupuesto"; */
+        const subject = field === 'email' ? 'The email' : 'The password';
 
-        //if (field == "date") return subject + " es obligatoria.";
-        if (errors["required"]) return subject + " is mandatory.";
-        // if (errors["minlength"])
-        //     return (
-        //         subject +
-        //         ` debe tener al menos ${errors["minlength"].requiredLength} caracteres.`
-        //     );
-        // if (errors["budgetExists"]) return subject + " ya existe.";
-        return subject + " is not valid.";
+        if (errors['required']) return subject + ' is mandatory.';
+        if (errors['minlength'])
+            return (
+                subject +
+                ` must be at lest ${errors['minlength'].requiredLength} characters.`
+            );
+        if (errors['checkPassword'])
+            return (
+                subject +
+                ' The password must have at least one lowercase letter, one uppercase letter, one digit, and one special character.'
+            );
+        return subject + ' is not valid.';
     };
 
     login() {
-        //const users = localStorage.getItem("users");
-        const modalRef = this.modalService.open(SignupComponent, {
-            fullscreen: true,
-            windowClass: "login-modal",
+        const email = this.loginForm.get('email')?.value;
+        const password = this.loginForm.get('password')?.value;
+
+        this.usersService.login(email, password).then((res) => {
+            if (res == 'signup') {
+                //this.activeModal.close();
+                this.modalService
+                    .open(this.confirmationRef)
+                    .result.then(
+                        (result) => {
+                            if (result == 'signup') {
+                                const modalRef = this.modalService.open(
+                                    SignupComponent,
+                                    {
+                                        fullscreen: true,
+                                        windowClass: 'login-modal',
+                                    }
+                                );
+                                this.user.email =
+                                    this.loginForm.get('email')?.value;
+                                this.user.password =
+                                    this.loginForm.get('password')?.value;
+                                modalRef.componentInstance.user = this.user;
+                                this.activeModal.close();
+                            }
+                        },
+                        (reason) => {
+                            console.log(reason);
+                        }
+                    );
+                return;
+            } else if (!res){
+                this.modalService
+                    .open(this.errorRef)
+                    .result.then(
+                        (result) => {
+                            console.log(result);
+
+                        },
+                        (reason) => {
+                            console.log(reason);
+                        }
+                    );
+            }
+            this.activeModal.close();
         });
-        const user: IUser = { email: this.loginForm.get("email")?.value };
-        modalRef.componentInstance.user = user;
-        // modalRef.componentInstance.passEntry.subscribe((receivedEntry: string) => {
-        //     console.log(receivedEntry);
-        // });
-        modalRef.result
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                //console.error(err);
-                this.activeModal.dismiss("Cross click");
-            });
+    }
+
+    togglePwdState() {
+        this.pwdState.state = 1 - this.pwdState.state;
     }
 }
