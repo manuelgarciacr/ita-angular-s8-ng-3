06099ace5b05afed2ff32dfa6900b21a1a0747a5
Manuel García Criado (laptop/linux) <manuel.garcia.cr@hotmail.com>
2023-10-02 05:10:57 +0200

    Level 1 Exercises 6 & 7. Completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive, Router } from '@angular/router';
 import { NgbCollapseModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgOptimizedImage } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { UsersService } from 'src/services/users.service';
+import { IUser } from 'src/model/IUser';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: "app-root",
+    selector: 'app-root',
     standalone: true,
     imports: [
         RouterOutlet,
@@ -14,21 +17,40 @@ import { LoginComponent } from './login/login.component';
         NgbCollapseModule,
         NgOptimizedImage,
     ],
-    templateUrl: "./app.component.html",
-    styleUrls: ["./app.component.scss"],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = "IT Academy Sprint 8";
+    title = 'IT Academy Sprint 8';
     isMenuCollapsed = true;
+    protected user!: IUser | null;
+    private subs: Subscription = this.usersService
+        .getUser()
+        .subscribe((user) => {
+            this.user = user;
+        });
 
-    constructor(private modalService: NgbModal) {}
+    constructor(
+        private modalService: NgbModal,
+        private usersService: UsersService,
+        private router: Router
+    ) {}
 
     openLogin() {
-        const modalRef = this.modalService.open(LoginComponent, { fullscreen: true, windowClass: "login-modal" });
+        const modalRef = this.modalService.open(LoginComponent, {
+            fullscreen: true,
+            windowClass: 'login-modal',
+        });
         modalRef.result
-            .then(res => {
-                console.log(res)
+            .then((res) => {
+                console.log(res);
             })
-            .catch(err => console.error(err));
+            .catch((err) => console.error(err));
+    }
+
+    logout() {
+        this.usersService.logout();
+        if (this.router.url == "/starships")
+            this.router.navigate(["/"]);
     }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,78 +1,141 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { JsonPipe, NgIf } from '@angular/common';
 import { IUser } from 'src/model/IUser';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from '../login/login.component';
+import { checkPasswordValidator } from 'src/utils/CustomValidators';
+import { UsersService } from 'src/services/users.service';
 
 @Component({
-    selector: "app-signup",
+    selector: 'app-signup',
     standalone: true,
     imports: [NgIf, JsonPipe, ReactiveFormsModule],
-    templateUrl: "./signup.component.html",
+    templateUrl: './signup.component.html',
     styles: [
-        ".close-btn svg { width: 1.4em; height: 1.4em}",
-        ":host { min-height: 100vh; display: flex; flex-direction: column}",
+        '.close-btn svg { width: 1.4em; height: 1.4em}',
+        ':host { min-height: 100vh; display: flex; flex-direction: column}',
     ],
 })
 export class SignupComponent implements OnInit {
-    @Input() protected user: IUser = { email: "" };
     protected signUpForm: FormGroup = this.formBuilder.group({});
+    private _user: IUser = { email: '' };
+    @Input()
+    set user(value: IUser) {
+        this._user = value;
+    }
+    get user(): IUser {
+        return this._user;
+    }
+    protected pwdState = {
+        type: ['password', 'text'],
+        svg: ['eye-slash', 'eye'],
+        alt: [
+            'The password text is not visible',
+            'The password text is visible',
+        ],
+        state: 0,
+    };
+    protected tabindex = '-1';
+    protected signUpError = true;
+    @ViewChild('confirmation', { static: true }) confirmation:
+        | ElementRef
+        | undefined;
 
     constructor(
+        public activeModal: NgbActiveModal,
         private formBuilder: FormBuilder,
-        public activeModal: NgbActiveModal
+        private modalService: NgbModal,
+        private usersService: UsersService
     ) {}
 
     ngOnInit(): void {
         const formGroup = {
-            firstName: new FormControl("", {
-                validators: [Validators.required],
-                // asyncValidators: [
-                //     this.budgetExistsValidator.validate.bind(
-                //         this.budgetExistsValidator
-                //     ),
-                // ],
+            firstName: new FormControl('', {
+                validators: [Validators.required, Validators.minLength(5)],
             }),
+            lastName: new FormControl('', {
+                validators: [Validators.required, Validators.minLength(5)],
+            }),
+            password: new FormControl('', {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(8),
+                    checkPasswordValidator(),
+                ],
+            }),
+            mailing: new FormControl(false)
         };
         this.signUpForm = this.formBuilder.group(
-            formGroup /* , {
-            validators: (control: AbstractControl) => {
-                const website = control.get("website")?.value;
-                const pages = control.get("pages")?.value;
-                const languages = control.get("languages")?.value;
-                if (!website) return null;
-                if (pages > 0 && languages > 0) return null;
-                return { detail: true };
-            },
-        } */
+            formGroup
         );
+        this.signUpForm.reset();
+        if (this.user.password != null)
+            this.signUpForm.get('password')!.setValue(this.user.password);
+        setTimeout(() => (this.tabindex = '0'), 500);
     }
 
     protected getError = (field: string) => {
         const errors = this.signUpForm.get(field)?.errors ?? {};
-        const subject = "The first name";
-        /* field === "name"
-                ? "El nombre del presupuesto"
-                : field === "customer"
-                ? "El nombre del cliente"
-                : "La fecha del presupuesto"; */
+        const subject =
+            field === 'firstName'
+                ? 'The first name'
+                : field === 'lastName'
+                ? 'The last name'
+                : 'The password';
 
-        //if (field == "date") return subject + " es obligatoria.";
-        if (errors["required"]) return subject + " is mandatory.";
-        // if (errors["minlength"])
-        //     return (
-        //         subject +
-        //         ` debe tener al menos ${errors["minlength"].requiredLength} caracteres.`
-        //     );
-        // if (errors["budgetExists"]) return subject + " ya existe.";
-        return subject + " is not valid.";
+        if (errors['required']) return subject + ' is mandatory.';
+        if (errors['minlength'])
+            return (
+                subject +
+                ` must be at lest ${errors['minlength'].requiredLength} characters.`
+            );
+        if (errors['checkPassword'])
+            return (
+                subject +
+                ' The password must have at least one lowercase letter, one uppercase letter, one digit, and one special character.'
+            );
+        return subject + ' is not valid.';
     };
 
     closeSignup() {
-        console.log("CLOSE SIGNUP")
+        const modalRef = this.modalService.open(LoginComponent, {
+            fullscreen: true,
+            windowClass: 'login-modal',
+        });
+        modalRef.componentInstance.user = this.user;
+        this.activeModal.close();
     }
 
     signUp() {
-        alert("SIGNUP");
+        const user = this.user;
+        user.firstName = this.signUpForm.get('firstName')?.value;
+        user.lastName = this.signUpForm.get('lastName')?.value;
+        user.password = this.signUpForm.get('password')?.value;
+        user.mailing = this.signUpForm.get('mailing')?.value;
+
+        if (!user.mailing)
+            user.mailing = false;
+
+        this.usersService.signup(user).then((res) => {
+            console.log(res);
+            if (res == 'login') {
+                this.signUpError = true;
+            } else {
+                this.signUpError = false;
+            }
+            this.modalService
+                .open(this.confirmation)
+                .result.then(
+                    (result) => {console.log(result);},
+                    (reason) => {console.log(reason);}
+                );
+            this.activeModal.close();
+            return;
+        });
+    }
+
+    togglePwdState() {
+        this.pwdState.state = 1 - this.pwdState.state;
     }
 }
